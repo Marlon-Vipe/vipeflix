29e30296f7bec6615d083d9d61278e6993cceed7
Marlon Villalona <iamvillalona@gmail.com>
2024-12-31 18:54:24 -0400

    add validation in generic-list component

--- vipeflixAPI/obj/Debug/net9.0/vipeflixAPI.AssemblyInfo.cs
+++ vipeflixAPI/obj/Debug/net9.0/vipeflixAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vipeflixAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8c3321009adfb090aecf2b6c392736327537a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e6a843df3d1c2d41afff7fb402b6d3a319628a")]
 [assembly: System.Reflection.AssemblyProductAttribute("vipeflixAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vipeflixAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
