586044043a999f8cb683f8c3316edcfcd5c609df
Marlon Villalona <iamvillalona@gmail.com>
2025-01-04 22:27:42 -0400

    update package.json

--- vipeflixAPI/obj/Debug/net9.0/vipeflixAPI.AssemblyInfo.cs
+++ vipeflixAPI/obj/Debug/net9.0/vipeflixAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vipeflixAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a15e9ff18b6277fe6c30cdd331412f40051f93e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82fead59d61d901e8fff4263a83aa19b60e4cdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("vipeflixAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vipeflixAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
